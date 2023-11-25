4dd83015d894a4542462995800a0603fac62b894
Ayomii1 <152004472+Ayomii1@users.noreply.github.com>
2023-11-25 13:29:51 +0100

    Add files via upload

---
+++ Zahidat Alao Hertech/script.js
@@ -0,0 +1,55 @@
+const chance = document.getElementById("image1");
+
+chance.addEventListener("click", (event) =>{
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const chance1 = document.getElementById("image2");
+
+chance1.addEventListener("click", (event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "pink";
+});
+
+const chance2 = document.getElementById("image3");
+
+chance2.addEventListener("click", (event) =>{
+    event.target.style.background = "pink";
+    event.target.style.color = "green";
+});
+
+const chance3 = document.getElementById("image4");
+
+chance3.addEventListener("click", (event) =>{
+    event.target.style.background = "yellow";
+    event.target.style.color = "blue";
+});
+
+const chance4 = document.getElementById("image5");
+
+chance4.addEventListener("click", (event) =>{
+    event.target.style.background = "brown";
+    event.target.style.color = "black";
+});
+
+const chance5 = document.getElementById("image6");
+
+chance5.addEventListener("click", (event) =>{
+    event.target.style.background = "blue";
+    event.target.style.color = "yellow";
+});
+
+const chance6 = document.getElementById("image7");
+
+chance6.addEventListener("click", (event) =>{
+    event.target.style.background = "gray";
+    event.target.style.color = "brown";
+});
+
+const chance7 = document.getElementById("image8");
+
+chance7.addEventListener("click", (event) =>{
+    event.target.style.background = "orange";
+    event.target.style.color = "purple";
+});
